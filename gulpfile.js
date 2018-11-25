3fe06dc9cbca5336535d14f341f324bdd3c6b598
baban <babanba.n@gmail.com>
2018-11-25 14:41:31 +0900

    documentcssを監視対象に入れる

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,15 @@ var autoprefixer = require('gulp-autoprefixer');
 // error handling
 var plumber = require('gulp-plumber');
 
-gulp.task('sass', function() {
+gulp.task('document-css', function() {
+    gulp.src(path.join(assetsPath, 'document.scss'))
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(path.join(assetsPath, './')));
+});
+
+gulp.task('seminor-css', function() {
     gulp.src(path.join(assetsPath, 'seminor.scss'))
         .pipe(plumber())
         .pipe(sass())
@@ -22,6 +30,16 @@ gulp.task('sass', function() {
         .pipe(gulp.dest(path.join(assetsPath, './')));
 });
 
+gulp.task('programs-css', function() {
+    gulp.src(path.join(assetsPath, 'programs.scss'))
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(path.join(assetsPath, './')));
+});
+
 gulp.task('default', function() {
-    gulp.watch(path.join(assetsPath, '*.scss'),['sass']);
+    gulp.watch(path.join(assetsPath, '*.scss'),['document-css']);
+    gulp.watch(path.join(assetsPath, '*.scss'),['programs-css']);
+    gulp.watch(path.join(assetsPath, '*.scss'),['seminor-css']);
 });
